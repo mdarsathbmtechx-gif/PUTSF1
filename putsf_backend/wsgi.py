ea25831f8eedae58859bb4a00dcfd121c0ddb608
mdarsathbmtechx-gif <mdarsath.bmtechx@gmail.com>
2025-09-30 22:14:54 +0530

    Fix wsgi.py for Python 3.11 compatibility

--- putsf_backend/wsgi.py
+++ putsf_backend/wsgi.py
@@ -4,15 +4,18 @@ WSGI config for putsf_backend project.
 It exposes the WSGI callable as a module-level variable named ``application``.
 
 For more information on this file, see
-https://docs.djangoproject.com/en/5.2/howto/deployment/wsgi/
+https://docs.djangoproject.com/en/4.2/howto/deployment/wsgi/
 """
+
 import sys
-print(sys.version)
+import types
+
+# Temporary cgi module stub for old Django
+import builtins
+sys.modules['cgi'] = types.ModuleType('cgi')
 
 import os
-
 from django.core.wsgi import get_wsgi_application
 
 os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'putsf_backend.settings')
-
 application = get_wsgi_application()
