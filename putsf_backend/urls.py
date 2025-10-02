14b52046a9fe0590071f36d9d9006d2decc3460b
mdarsathbmtechx-gif <mdarsath.bmtechx@gmail.com>
2025-10-02 17:25:32 +0530

    Add homepage view and update URLs

--- putsf_backend/urls.py
+++ putsf_backend/urls.py
@@ -2,15 +2,16 @@ from django.contrib import admin
 from django.urls import path, include
 from django.conf import settings
 from django.conf.urls.static import static
+from .views import home  # Add this
 
 urlpatterns = [
+    path("", home, name="home"),  # root path
     path("admin-django/", admin.site.urls),
-    path("api/admin/", include("putsf_backend.accounts.urls")),   # ✅ full path
-    path('api/gallery/', include('putsf_backend.gallery.urls')),  # ✅ full path
-    path("api/", include("putsf_backend.banner.urls")),           # ✅ full path
-    path("api/blog/", include("putsf_backend.blog.urls")),  
-    ]
-
+    path("api/admin/", include("putsf_backend.accounts.urls")),
+    path("api/gallery/", include("putsf_backend.gallery.urls")),
+    path("api/", include("putsf_backend.banner.urls")),
+    path("api/blog/", include("putsf_backend.blog.urls")),
+]
 
 if settings.DEBUG:
     urlpatterns += static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
