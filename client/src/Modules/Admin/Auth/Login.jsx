3c4fe6d905ac0a32fa7ec0d2d0ef25066717c169
mdarsathbmtechx-gif <mdarsath.bmtechx@gmail.com>
2025-10-02 21:02:46 +0530

    Update admin login to use deployed backend

--- client/src/Modules/Admin/Auth/Login.jsx
+++ client/src/Modules/Admin/Auth/Login.jsx
@@ -17,7 +17,7 @@ export default function Login() {
 
     try {
       const res = await axios.post(
-        "http://127.0.0.1:8000/api/admin/login/",
+        import.meta.env.VITE_API_BASE_URL + "/admin/login/",
         { email, password },
         { headers: { "Content-Type": "application/json" } }
       );
