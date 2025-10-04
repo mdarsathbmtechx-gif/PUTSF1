95e3920b7c0b107dc83d4f69b6111b4e0969a423
mdarsathbmtechx-gif <mdarsath.bmtechx@gmail.com>
2025-10-04 11:15:27 +0530

    Move images to static folder and update Blog model for static storage

--- putsf_backend/blog/models.py
+++ putsf_backend/blog/models.py
@@ -1,10 +1,9 @@
-# blog/models.py
 from django.db import models
 from django.core.files.storage import FileSystemStorage
 from django.conf import settings
 import os
 
-# Force images to static/blog instead of media/blog
+# storage pointing to static/blog
 blog_storage = FileSystemStorage(
     location=os.path.join(settings.BASE_DIR, "putsf_backend", "static", "blog"),
     base_url="/static/blog/"
@@ -14,6 +13,3 @@ class Blog(models.Model):
     title = models.CharField(max_length=255)
     image = models.ImageField(upload_to='', storage=blog_storage, blank=True, null=True)
     created_at = models.DateTimeField(auto_now_add=True)
-
-    def __str__(self):
-        return self.title
